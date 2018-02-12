d84e84835a6c40a3cb52dabd9ebb1269133f97a9
John Castillo <JohnluisCastillo@gmail.com>
2018-02-12 10:00:59 -0500

    index

---
+++ src/store.js
@@ -0,0 +1,18 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {reducer as formReducer} from 'redux-form';
+import thunk from 'redux-thunk';
+import authReducer from './reducers/auth';
+import protectedDataReducer from './reducers/protected-data';
+
+const store = createStore(
+    combineReducers({
+        form: formReducer,
+        auth: authReducer,
+        protectedData: protectedDataReducer
+    }),
+    applyMiddleware(thunk)
+);
+
+// Hydrate the authToken from localStorage if it exist
+
+export default store;
